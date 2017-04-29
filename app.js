952ff0482e043b8313c1e2cc87a33ccc2fe63e95
Hannah Reinbolt <hannah@master-logic.com>
2017-04-29 13:46:42 -0500

    upp

--- app.js
+++ app.js
@@ -3,39 +3,46 @@
 const keyPublishable = "pk_test_6pRNASCoBOKtIshFeQd4XMUh";
 const keySecret = "sk_test_BQokikJOvBiI2HlWgH4olfQ2";
 
-const bodyParser = require("express");
+const express = require("express");
+const stripe = require("stripe")(keySecret);
+const html = require("html");
 const bodyParser = require("body-parser");
 
-const app = express();
-app.use(express.static("public"));
+let app = express();
 
+app.set('view engine', 'html');
+app.set("/", __dirname + "/");
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) =>
-  res.render("index.html", {keyPublishable}));
-
-app.post("/charge", (req, res) => {
-  let amount = 500;
-  
-  // Token is created using Stripe.js or Checkout!
-// Get the payment token submitted by the form:
-var token = request.body.stripeToken.id; // Using Express
-
-// Create a Charge:
-stripe.charges.create({
-  amount,
-  currency: "usd",
-  source: token,
-}, {
-  stripe_account: "{CONNECTED_STRIPE_ACCOUNT_ID}",
-})
-  .then(charge => res.send(charge))
-  .catch(err => {
-    console.log("Error:", err);
-    res.status(500).send({error: "Purchase Failed"});
-  });
-  console.log("Success!");
+app.get("/", function(req, res){
+    res.render("index",{
+    });
+}); 
+app.get("/paysuccess", function(req, res){
+    res.render("paysuccess",{
+    });
 });
 
-app.listen(8000);
+app.post('/charge', function(req, res) {
+    console.log(JSON.stringify(req.body, null, 2));
+    var stripeToken = req.body.token.id;
+
+    var charge = stripe.charges.create({
+        amount: 5000, // amount in cents, again
+        currency: "usd",
+        source: stripeToken,
+        description: "payinguser@example.com"
+    }, function(err, charge) {
+        if (err && err.type === 'StripeCardError') {
+            console.log(JSON.stringify(err, null, 2));
+        }
+    });
+    console.log("your payment was a success!")
+    res.redirect("/paysuccess")
+});
+
+app.listen(8000, function(){
+    console.log("stripe is running");
+});
